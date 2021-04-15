b21427bcff00dff4ccd93da609c3bcb3181f4230
Eric Chiu <eric840906@hotmail.com.tw>
2021-04-15 09:22:31 +0800

    init

---
+++ js/About.6121586d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["About"],{8727:function(e,n,o){"use strict";o.r(n);var c=o("7a23"),a={class:"about page-view"};function t(e,n,o,t,r,i){return Object(c["openBlock"])(),Object(c["createBlock"])("div",a)}var r=o("c428"),i={component:{Loading:r["a"]}};i.render=t;n["default"]=i}}]);
+//# sourceMappingURL=About.6121586d.js.map
